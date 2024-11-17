828b0cdd60610f0381f7de4241ed1c04c7c04456
Edoardo La Greca <edo@edolg.net>
2024-11-17 21:29:13 +0100

    add test.sh

---
+++ test.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# Test setup.sh and unsetup.sh.
+
+# make a snapshot of all existing file paths
+# $1 = directory of which to take the snapshot
+# $2 = name of the output file
+snapfs() {
+	du -a "$1" | awk '{ print $2 }' | sort >"$2"
+}
+
+files="{,un}setup.sh"
+ftp https://raw.githubusercontent.com/EdoardoLaGreca/werc-on-openbsd/main/$files
+sha256 $files
+sed -i "s/^domain='.*'$/domain='mysite.lol'/" $files
+grep 'mysite\.lol' $files
+chmod 744 $files
+snapfs /etc etc.bef
+snapfs /var/www www.bef
+
+setup() {
+	doas ./setup.sh
+	snapfs /etc etc.aft1
+	snapfs /var/www www.aft1
+}
+
+unsetup() {
+	doas ./unsetup.sh
+	snapfs /etc etc.aft2
+	snapfs /var/www www.aft2
+}
+
+"$1"
